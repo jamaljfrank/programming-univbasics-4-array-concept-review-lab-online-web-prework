9d8c2e0b175dba5f79e613ecd8c092b811c8f39e
Jamal Frank <jamaljfrank@gmail.com>
2019-07-14 23:01:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,10 +5,11 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  puts find_max_value([array])
+  puts find_max_value(array)
   
 end
 
 def find_min_value(array)
-  puts find_min_value([array])
+  new_array = array.sort 
+  new_array()
 end
