1f156ba349a9df59c73de36359b7604912e98fa5
Jamal Frank <jamaljfrank@gmail.com>
2019-07-14 22:53:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,13 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  puts array[1]
+
 end
 
 def find_max_value(array)
-  # Add your solution here
+  puts find_max_value([array])
+  
 end
 
 def find_min_value(array)
-  # Add your solution here
+  puts find_min_value([array])
 end
